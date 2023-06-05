cd9469d385cf83370a1f0f52e099bc3d66325eaf
Macwin16 <59633966+Macwin16@users.noreply.github.com>
2023-06-04 20:02:41 -0500

    Add files via upload

---
+++ EtchASketch/EtchASketchJS.js
@@ -0,0 +1,65 @@
+let color ='black';
+let click = true;
+
+function populateBoard(size) {
+    let board = document.querySelector(".board");
+    let squares = board.querySelectorAll("div")
+    squares.forEach((div) => div.remove());
+    board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
+
+    let amount = size * size;
+    for (let i=0; i < amount; i++){
+        let square = document.createElement("div");
+        square.addEventListener("mouseover",colorSquare)
+        square.style.backgroundColor = 'white';
+        board.insertAdjacentElement("beforeend", square);
+    }
+}
+
+populateBoard(16);
+
+function changeSize(input){
+    if(input >= 2 && input <= 100){
+        document.querySelector(".error").style.display = 'none';
+        populateBoard(input);
+    }
+    else{
+    document.querySelector(".error").style.display = 'flex';
+    }
+
+}
+
+function colorSquare() {
+    if (click) {
+
+        if(color ==='random') {
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+        } else{
+            this.style.backgroundColor = color;
+        }
+    }   
+}
+
+function changeColor(choice) {
+    color = choice;
+}
+
+function resetBoard() {
+    let board = document.querySelector(".board");
+    let squares = board.querySelectorAll("div");
+    squares.forEach((div) => div.style.backgroundColor = 'white');
+}
+
+document.querySelector('body').addEventListener("click", (event) =>{
+    if(event.target.tagName != 'BUTTON'){
+            click = !click;
+        if (click) {
+            document.querySelector('.mode').textContent = "mode: Coloring"
+        } else{
+            document.querySelector('.mode').textContent = "mode: not Coloring"
+        }
+    }
+} ) // event listener, if event is true, set to false. Boolean value has been assigned.
+    // if you click on a button,ex. Black Erasor Gray, the mode will not reset to coloring, 
+    //it will stay on not Coloring the way you had it
